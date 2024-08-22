c6e34ece0c64dfce8276144c63f14ccc105ec9e3
coleYab <yabume123@gamil.com>
2024-08-22 11:14:34 +0300

    0-uniq_users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creating users with unique email and auto incremented id.
 
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    name VARCHAR(255)
 );
